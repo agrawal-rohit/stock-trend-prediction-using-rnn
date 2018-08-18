0aadedfb9305d32bce70f41ef5d2a082e1e40cad
Rohit Agrawal <rohitagrawalofficialmail@gmail.com>
2018-08-18 10:02:21 +0530

    Updated Comments

--- rnn.py
+++ rnn.py
@@ -1,17 +1,17 @@
-# Recurrent Neural Networks
-
-# Part 1 : Data Processing( DIfferent from ANN one)
-
-#libs
+# Importing libs
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
 
-#importing dataset
+# Importing dataset
 dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
 training_set = dataset_train.iloc[:, 1:2].values # upper bound of range is excluded, put in range as we need the data as a numpy array which can be put into the network, else it will be a single vector
 
-#Feature scaling
+# Feature scaling
 from sklearn.preprocessing import MinMaxScaler
 sc = MinMaxScaler(feature_range=(0,1)) # Normal Scaler, Better here since its an RNN and since we have sigmoid function as activation function in the output layer
 training_set_scaled = sc.fit_transform(training_set)
@@ -26,54 +26,31 @@ for i in range(60,1258): # 60th row to last row
 
 x_train,y_train = np.array(x_train), np.array(y_train)
 
-# Reshaping (used when you want to include other predictors too)
-x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1)) # The RNN takes the input as as a 3D tensor with following params => (batch_size or total number of obvs, timesteps, input_dims or no. of predictors)
+# Reshaping
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1)) 
 
-# Part 2 : Building the RNN
-#--------------------------
 
-#libs
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from keras.layers import Dropout
-
-# initializing the RNN
+# Building the RNN
 regressor = Sequential()
-
-#Adding first LSTM layer and Dropout regularization
 regressor.add(LSTM(units = 50, return_sequences = True, input_shape=(x_train.shape[1],1))) # keep units value high to implement high dimensionality
 regressor.add(Dropout(0.2))
-
-#Adding second LSTM layer and Dropout
 regressor.add(LSTM(units = 50, return_sequences = True))
 regressor.add(Dropout(0.2))
-
-#Adding third LSTM layer and Dropout
 regressor.add(LSTM(units = 50, return_sequences = True))
 regressor.add(Dropout(0.2))
-
-#Adding fourth LSTM layer and Dropout
 regressor.add(LSTM(units = 50))
 regressor.add(Dropout(0.2))
-
-#Adding the output layer
 regressor.add(Dense(units = 1))
-
-#Compiling
 regressor.compile(optimizer = 'adam', loss='mean_squared_error')
-
-#Fitting
 regressor.fit(x_train,y_train,epochs = 100, batch_size = 32)
 
-# Part 3 : Making the predictions and visualizing the results
-#------------------------------------------------------------
+# Making the predictions and visualizing the results
  
-# Getting real stock price of 2017
+# Ground Truth
 dataset_test = pd.read_csv('Google_Stock_Price_Test.csv')
 real_stock_price = dataset_test.iloc[:, 1:2].values
 
-# Getting predicted stock price of 2017
+# Our predictions
 dataset_total = pd.concat((dataset_train['Open'],dataset_test['Open']),axis = 0)
 inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
 inputs = inputs.reshape(-1,1)
